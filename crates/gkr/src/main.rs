2d1cafd80d0780c2e88ad1d68f49a5ffef9b554d
Xander van der Goot <xandervandergoot@gmail.com>
2026-08-25 13:50:16 +0800

    Wrap the suffix eq-table in a SuffixTable struct with pop()

--- crates/gkr/src/main.rs
+++ crates/gkr/src/main.rs
@@ -14,31 +14,43 @@ fn prove_layer(challenges: &[Field]) {}
 
 fn gpgkr_verify() {}
 
-// Can be allocated as a single table directly.
-// Maybe vectors can be reused?
-// Might be better in combination with split_eq table
-fn suffix_table(point: &[Field]) -> Vec<Vec<Field>> {
-    // We do not need to build the table for the first challenge.
-    let mut table = Vec::with_capacity(point.len() - 1);
-    let mut iter = point.iter().skip(1).rev();
-    let last = iter.next().unwrap();
-    let mut prev = Vec::from([(1 - *last), *last]);
+struct SuffixTable(Vec<Vec<Field>>);
 
-    for z in iter {
-        let size = prev.len() << 1;
-        let mut entry = vec![0; size];
-        let (low, hi) = entry.split_at_mut(size >> 1);
+impl SuffixTable {
+    // This option might get annoying
+    // Can be allocated as a single table directly.
+    // Maybe vectors can be reused? This could be reused with sumcheck layer. The evaluation layer can be reused each gkr
+    // Might be better in combination with split_eq table
+    // currently has to be consumed in reverse order -> pop. Which is fine
+    fn new(point: &[Field]) -> SuffixTable {
+        // We do not need to build the table for the first challenge.
+        let mut table = Vec::with_capacity(point.len() - 1);
+        let mut iter = point.iter().skip(1).rev();
+        let last = iter.next().unwrap();
+        let mut prev = Vec::from([(1 - *last), *last]);
 
-        // Does the lead to a range check?
-        for (i, e) in prev.iter().enumerate() {
-            (low[i], hi[i]) = ((1 - z) * e, z * e)
+        for z in iter {
+            let size = prev.len() << 1;
+            let mut entry = vec![0; size];
+            let (low, hi) = entry.split_at_mut(size >> 1);
+
+            // Does the lead to a range check?
+            for (i, e) in prev.iter().enumerate() {
+                let tmp = z * e;
+                // (1-z)*e, z*e
+                (low[i], hi[i]) = (e - tmp, tmp)
+            }
+
+            table.push(prev);
+            prev = entry;
         }
-
         table.push(prev);
-        prev = entry;
+        SuffixTable(table)
     }
-    table.push(prev);
-    table
+
+    fn pop(&mut self) -> Option<Vec<Field>> {
+        self.0.pop()
+    }
 }
 
 type Field = i32;
